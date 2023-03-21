75cd6d3aea6e1c3b014b877e9463976937e49af9
Natalia yatsishin <nataliayatsishin@Natalias-MacBook-Air.local>
2023-03-21 17:27:19 -0500

    Hello

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("sdcfghjkl");
+        System.out.println("12");
+        System.out.println("123456789");
+    }
+}
